a8d3b2b54388d7eaa308765fc360987311cd69a3
rpnsnoeren <rpn.snoeren@gmail.com>
2024-11-20 23:32:18 +0100

    2435

--- server.js
+++ server.js
@@ -1,28 +1,49 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 
-// Serve static files
-app.use(express.static('src'));
+// Enable CORS
+app.use(cors());
+
+// Serve static files from the src directory
+app.use(express.static(path.join(__dirname, 'src')));
 
 // Set correct MIME types
 app.use((req, res, next) => {
-    if (req.url.endsWith('.js')) {
-        res.type('application/javascript');
-    } else if (req.url.endsWith('.css')) {
-        res.type('text/css');
-    } else if (req.url.endsWith('.html')) {
-        res.type('text/html');
+    const ext = path.extname(req.url);
+    switch (ext) {
+        case '.js':
+            res.type('application/javascript');
+            break;
+        case '.css':
+            res.type('text/css');
+            break;
+        case '.html':
+            res.type('text/html');
+            break;
     }
     next();
 });
 
-// Handle all routes
+// Handle component requests
+app.get('/components/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', req.path));
+});
+
+// Handle all other routes by serving index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log(`Server root directory: ${__dirname}`);
 }); 
